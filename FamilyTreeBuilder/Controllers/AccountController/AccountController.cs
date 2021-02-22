8601376272cf6faed1e962fb9b3e656040989b69
Anderson Lemos <a.lemos@nordware.io>
2021-02-22 10:01:03 -0300

    Primeiro Commit

---
+++ FamilyTreeBuilder/Controllers/AccountController/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Okta.AspNetCore;
+
+namespace FamilyTreeBuilder.Controllers.AccountController
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            if (!HttpContext.User.Identity.IsAuthenticated)
+            {
+                return Challenge(OktaDefaults.MvcAuthenticationScheme);
+            }
+            return RedirectToAction("Index", "People");
+        }
+
+        public IActionResult Logout()
+        {
+            return new SignOutResult(new[]
+            {
+        OktaDefaults.MvcAuthenticationScheme,
+        CookieAuthenticationDefaults.AuthenticationScheme
+      });
+        }
+    }
+}
---
+++ FamilyTreeBuilder/ModelsGenerated/FamilyTreeContext.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FamilyTreeBuilder.ModelsGenerated
+{
+    public partial class FamilyTreeContext : DbContext
+    {
+        public FamilyTreeContext()
+        {
+        }
+
+        public FamilyTreeContext(DbContextOptions<FamilyTreeContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Person> Person { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("Server=motty.db.elephantsql.com;Database=zfmlybhk;User Id=zfmlybhk;Password=6TR1RFCwMtqsN14Vx3--5UP9EYSyjIns;Port=5432");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasPostgresExtension("btree_gin")
+                .HasPostgresExtension("btree_gist")
+                .HasPostgresExtension("citext")
+                .HasPostgresExtension("cube")
+                .HasPostgresExtension("dblink")
+                .HasPostgresExtension("dict_int")
+                .HasPostgresExtension("dict_xsyn")
+                .HasPostgresExtension("earthdistance")
+                .HasPostgresExtension("fuzzystrmatch")
+                .HasPostgresExtension("hstore")
+                .HasPostgresExtension("intarray")
+                .HasPostgresExtension("ltree")
+                .HasPostgresExtension("pg_stat_statements")
+                .HasPostgresExtension("pg_trgm")
+                .HasPostgresExtension("pgcrypto")
+                .HasPostgresExtension("pgrowlocks")
+                .HasPostgresExtension("pgstattuple")
+                .HasPostgresExtension("tablefunc")
+                .HasPostgresExtension("unaccent")
+                .HasPostgresExtension("uuid-ossp")
+                .HasPostgresExtension("xml2")
+                .HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.ToTable("person");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.BirthDate)
+                    .HasColumnName("birth_date")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.DataOwnerId).HasColumnName("data_owner_id");
+
+                entity.Property(e => e.DeathDate)
+                    .HasColumnName("death_date")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.Father).HasColumnName("father");
+
+                entity.Property(e => e.FirstName)
+                    .HasColumnName("first_name")
+                    .HasColumnType("character varying");
+
+                entity.Property(e => e.LastName)
+                    .HasColumnName("last_name")
+                    .HasColumnType("character varying");
+
+                entity.Property(e => e.Mother).HasColumnName("mother");
+
+                entity.HasOne(d => d.FatherNavigation)
+                    .WithMany(p => p.InverseFatherNavigation)
+                    .HasForeignKey(d => d.Father)
+                    .HasConstraintName("person_father_fkey");
+
+                entity.HasOne(d => d.MotherNavigation)
+                    .WithMany(p => p.InverseMotherNavigation)
+                    .HasForeignKey(d => d.Mother)
+                    .HasConstraintName("person_mother_fkey");
+            });
+        }
+    }
+}
